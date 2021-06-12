dffde4ecd6dfbfdd626369b5a25ccff69de48e5f
Viktor <ricchiuto.viktor@gmail.com>
2021-06-12 18:33:17 +0200

    style: improve scroll up button look in home page

--- client/src/pages/HomePage/ScrollUpButton/ScrollUpButton.tsx
+++ client/src/pages/HomePage/ScrollUpButton/ScrollUpButton.tsx
@@ -1,4 +1,5 @@
 import { GoBackIcon } from 'assets/icons';
+import { usePullDownOnHover } from 'hooks/usePullDownOnHover';
 import React from 'react';
 import './ScrollUpButton.scss';
 
@@ -6,12 +7,14 @@ import './ScrollUpButton.scss';
 // just above the footer
 
 export function ScrollUpButton() {
+  const { container } = usePullDownOnHover(true);
+
   function handleClick() {
     alert('not implemented');
   }
 
   return (
-    <div className="scroll-up-button" onClick={handleClick}>
+    <div className="scroll-up-button" onClick={handleClick} ref={container}>
       <GoBackIcon />
     </div>
   );
