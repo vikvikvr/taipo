90d4f0740f8926a0bcae7ec06df24db260715510
Viktor <ricchiuto.viktor@gmail.com>
2021-06-14 17:56:52 +0200

    feat: add scroll on click for button in home page

--- client/src/pages/HomePage/ScrollUpButton/ScrollUpButton.tsx
+++ client/src/pages/HomePage/ScrollUpButton/ScrollUpButton.tsx
@@ -2,6 +2,11 @@ import { GoBackIcon } from 'assets/icons';
 import { usePullDownOnHover } from 'hooks/usePullDownOnHover';
 import React from 'react';
 import './ScrollUpButton.scss';
+import { gsap } from 'gsap';
+import { ScrollToPlugin } from 'gsap/all';
+import { playSound } from 'services/audioService';
+
+gsap.registerPlugin(ScrollToPlugin);
 
 // floating button placed in the middle
 // just above the footer
@@ -10,7 +15,8 @@ export function ScrollUpButton() {
   const { container } = usePullDownOnHover(true);
 
   function handleClick() {
-    console.log('not implemented');
+    playSound('mouseClick');
+    gsap.to('.home-page', { scrollTo: 0 });
   }
 
   return (
